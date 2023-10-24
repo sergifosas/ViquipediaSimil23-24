8ae74cb086242919c70443a6fbfb32c31001a89d
Mateu Villaret <villaret@Mateus-MacBook.home>
2023-10-24 18:20:41 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,15 @@
+# ViquipediaSimil
+
+En aquest repositori hi trobareu una "llavor" per a poder usar/modifica el MapReduce vist a classe i també les llibreries de processament d'XML. A partir d'aquesta llavor podreu fer la vostra pràctica.
+
+Fixeu-vos que a la carpeta principal també hi teniu el fitxer **stopwordscatalanet.txt** amb català i un directori **viqui_files** amb els fitxers xml del fragment de la Viquipèdia a tractar (en el nostre cas són pàgines de com a mínim 5k que parlin de la segona guerra mundial, si algú prefereix un altre tema que s'actualizi el conjunt de pàgines amb el tema que vulgui).
+
+També podeu trobar l'enunciat detallat al document **practica2223b.pdf** i a la carpeta **primeraPartPractica** hi trobareu l'enunciat i els fitxers de la primera part de la pràctica.
+
+Per fer-ho correctament us recomano que aneu al **use this template** i creeu una còpia amb visibilitat **privada** amb el vostre nom de repositori.
+Pel lliurament em podeu donar permisos a mi.
+
+
+
+
+
---
+++ src/main/scala/main/Main.scala
@@ -0,0 +1,122 @@
+package main
+
+import java.io.File
+
+import akka.actor.{ActorSystem, PoisonPill, Props}
+import akka.pattern.ask
+import akka.util.Timeout
+import mapreduce._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+// Tenim dos objectes executables
+// - tractaxml utilitza un "protoParser" per la viquipèdia i
+// exampleMapreduce usa el MapReduce
+
+object fitxers extends App{
+  ProcessListStrings.mostrarTextDirectori("primeraPartPractica")
+}
+
+object tractaxml extends App {
+
+  val parseResult= ViquipediaParse.parseViquipediaFile()
+
+  parseResult match {
+    case ViquipediaParse.ResultViquipediaParsing(t,c,r) =>
+      println("TITOL: "+ t)
+      println("CONTINGUT: ")
+      println(c)
+      println("REFERENCIES: ")
+      println(r)
+  }
+}
+
+object exampleMapreduce extends App {
+
+  val nmappers = 1
+  val nreducers = 1
+  val f1 = new java.io.File("f1")
+  val f2 = new java.io.File("f2")
+  val f3 = new java.io.File("f3")
+  val f4 = new java.io.File("f4")
+  val f5 = new java.io.File("f5")
+  val f6 = new java.io.File("f6")
+  val f7 = new java.io.File("f7")
+  val f8 = new java.io.File("f8")
+
+  val fitxers: List[(File, List[String])] = List(
+    (f1, List("hola", "adeu", "per", "palotes", "hola","hola", "adeu", "pericos", "pal", "pal", "pal")),
+    (f2, List("hola", "adeu", "pericos", "pal", "pal", "pal")),
+    (f3, List("que", "tal", "anem", "be")),
+    (f4, List("be", "tal", "pericos", "pal")),
+    (f5, List("doncs", "si", "doncs", "quin", "pal", "doncs")),
+    (f6, List("quin", "hola", "vols", "dir")),
+    (f7, List("hola", "no", "pas", "adeu")),
+    (f8, List("ahh", "molt", "be", "adeu")))
+
+
+  val compres: List[(String,List[(String,Double, String)])] = List(
+    ("bonpeu",List(("pep", 10.5, "1/09/20"), ("pep", 13.5, "2/09/20"), ("joan", 30.3, "2/09/20"), ("marti", 1.5, "2/09/20"), ("pep", 10.5, "3/09/20"))),
+    ("sordi", List(("pep", 13.5, "4/09/20"), ("joan", 30.3, "3/09/20"), ("marti", 1.5, "1/09/20"), ("pep", 7.1, "5/09/20"), ("pep", 11.9, "6/09/20"))),
+    ("canbravo", List(("joan", 40.4, "5/09/20"), ("marti", 100.5, "5/09/20"), ("pep", 10.5, "7/09/20"), ("pep", 13.5, "8/09/20"), ("joan", 30.3, "7/09/20"), ("marti", 1.5, "6/09/20"))),
+    ("maldi", List(("pepa", 10.5, "3/09/20"), ("pepa", 13.5, "4/09/20"), ("joan", 30.3, "8/09/20"), ("marti", 0.5, "8/09/20"), ("pep", 72.1, "9/09/20"), ("mateu", 9.9, "4/09/20"), ("mateu", 40.4, "5/09/20"), ("mateu", 100.5, "6/09/20")))
+  )
+
+  // Creem el sistema d'actors
+  val systema: ActorSystem = ActorSystem("sistema")
+
+  // funcions per poder fer un word count
+  def mappingWC(file:File, words:List[String]) :List[(String, Int)] =
+        for (word <- words) yield (word, 1)
+
+
+  def reducingWC(word:String, nums:List[Int]):(String,Int) =
+        (word, nums.sum)
+
+
+  println("Creem l'actor MapReduce per fer el wordCount")
+  val wordcount = systema.actorOf(Props(new MapReduce(fitxers,mappingWC,reducingWC )), name = "mastercount")
+
+  // Els Futures necessiten que se'ls passi un temps d'espera, un pel future i un per esperar la resposta.
+  // La idea és esperar un temps limitat per tal que el codi no es quedés penjat ja que si us fixeu preguntar
+  // i esperar denota sincronització. En el nostre cas, al saber que el codi no pot avançar fins que tinguem
+  // el resultat del MapReduce, posem un temps llarg (100000s) al preguntar i una Duration.Inf a l'esperar la resposta.
+
+  // Enviem un missatge com a pregunta (? enlloc de !) per tal que inicii l'execució del MapReduce del wordcount.
+  //var futureresutltwordcount = wordcount.ask(mapreduce.MapReduceCompute())(100000 seconds)
+
+  implicit val timeout = Timeout(10000 seconds) // L'implicit permet fixar el timeout per a la pregunta que enviem al wordcount. És obligagori.
+  var futureresutltwordcount = wordcount ? mapreduce.MapReduceCompute()
+
+  println("Awaiting")
+  // En acabar el MapReduce ens envia un missatge amb el resultat
+  val wordCountResult:Map[String,Int] = Await.result(futureresutltwordcount,Duration.Inf).asInstanceOf[Map[String,Int]]
+
+
+  println("Results Obtained")
+  for(v<-wordCountResult) println(v)
+
+  // Fem el shutdown del actor system
+  println("shutdown")
+  systema.terminate()
+  println("ended shutdown")
+  // com tancar el sistema d'actors.
+
+  /*
+  EXERCICIS:
+
+  Useu el MapReduce per saber quant ha gastat cada persona.
+
+  Useu el MapReduce per saber qui ha fet la compra més cara a cada supermercat
+
+  Useu el MapReduce per saber quant s'ha gastat cada dia a cada supermercat.
+   */
+
+
+  println("tot enviat, esperant... a veure si triga en PACO")
+}
+
+
+
